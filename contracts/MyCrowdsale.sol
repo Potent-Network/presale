519d92c70528cefb6eb5c0bc20239975f14518e5
Jun Kim <junkim012@gmail.com>
2022-01-06 23:16:17 +0900

    add mintable crowdsale

--- contracts/MyCrowdsale.sol
+++ contracts/MyCrowdsale.sol
@@ -1,30 +1,80 @@
 pragma solidity ^0.5.0;
 
+import "hardhat/console.sol"; 
+
 import "@openzeppelin/contracts/crowdsale/Crowdsale.sol";
 import "@openzeppelin/contracts/crowdsale/emission/AllowanceCrowdsale.sol";
-import "@openzeppelin/contracts/crowdsale/validation/IndividuallyCappedCrowdsale.sol";
+import "@openzeppelin/contracts/crowdsale/emission/MintedCrowdsale.sol";
+import "@openzeppelin/contracts/crowdsale/validation/CappedCrowdsale.sol";
 import "@openzeppelin/contracts/crowdsale/validation/TimedCrowdsale.sol";
 
-contract MyCrowdsale is Crowdsale, AllowanceCrowdsale {
+// minimum investor contribution - 0.002 ether 
+// maximum investor contribution - 50 ether 
+contract MyCrowdsale is Crowdsale, MintedCrowdsale, CappedCrowdsale {
+
+    uint256 public investorMinCap = 2000000000000000;
+    uint256 public investorMaxCap = 50000000000000000000;
+    mapping(address => uint256) public contributions; 
+
+    //address private tokenWallet;
+    //AllowanceCrowdsale allowanceCrowdSale; 
     constructor(
         uint256 rate, 
         address payable wallet, 
         IERC20 token,
-        address tokenWallet
+        uint256 _cap
+        // address tokenWallet
         //address tokenWallet // argument for allowance crowdsale
         // IndividuallyCappedCrowdsale doesn't have a constructor 
         //uint256 openingTime, // for timed crowdsale
         //uint256 closingTime // for timed crowdsale 
 
     )
- 
     Crowdsale(rate, wallet, token) 
-    AllowanceCrowdsale(tokenWallet)
-
+    CappedCrowdsale(_cap)
     public
     {
 
     }
 
+    /**
+    * @dev Overrides AllowanceCrowdsale behavior by calling allowance/approve before transfer
+    * @param _beneficiary Token purchaser
+    * @param _weiAmount Amount of tokens sent
+    */
+    function _preValidatePurchase(
+        address _beneficiary,
+        uint256 _weiAmount
+    ) internal view {
+        super._preValidatePurchase(_beneficiary, _weiAmount); 
+        uint256 _existingContribution = contributions[_beneficiary]; 
+        uint256 _newContribution = _existingContribution.add(_weiAmount); 
+        require(_newContribution >= investorMinCap && _newContribution <= investorMaxCap);
+    }
+
+    function _updatePurchasingState(
+        address _beneficiary,
+        uint256 _weiAmount 
+    ) internal {
+        uint256 _existingContribution = contributions[_beneficiary]; 
+        uint256 _newContribution = _existingContribution.add(_weiAmount); 
+        contributions[_beneficiary] = _newContribution; 
+
+    }
+
+    // /**
+    // * @dev Overrides AllowanceCrowdsale behavior by calling allowance/approve before transfer
+    // * @param beneficiary Token purchaser
+    // * @param tokenAmount Amount of tokens purchased
+    // */
+    // function _deliverTokens(address beneficiary, uint256 tokenAmount) internal {
+    //     console.log("in MyCrowdsale _deliverTokens()"); 
+    //     console.log("beneficiary address: ", beneficiary); 
+    //     console.log("tokenAmount: ", tokenAmount); 
+    //     super.token().safeApprove(super.tokenWallet(), tokenAmount); 
+    //     super.token().safeIncreaseAllowance(super.tokenWallet(), tokenAmount); 
+
+    //     super.token().safeTransferFrom(super.tokenWallet(), beneficiary, tokenAmount);
+    // }
     // IERC20(tokenAddress).approve(CROWDSALE_ADDRESS, SOME_TOKEN_AMOUNT);
 }
--- test/crowdsale.test.js
+++ test/crowdsale.test.js
@@ -1,8 +1,10 @@
 
 
 const {web3} = require("hardhat"); 
+const ethers = require("ethers"); 
 
 const { contract } = require("hardhat");
+const { assert, expect } = require("chai");
 const BigNumber = web3.BigNumber;
 require('chai')
     .use(require('chai-as-promised'))
@@ -26,7 +28,8 @@ contract('Crowdsale', function([_, wallet, tokenWallet, investor1, investor2]) {
         this.name = "token"; 
         this.symbol = "TOK"; 
         this.decimals = 18; 
-        this.initialSupply = 100000;
+        this.initialSupply = ethers.BigNumber.from("1000000000000000000000");
+        console.log(this.initialSupply);
 
         // deploy token
         this.token = await Token.new(
@@ -39,20 +42,23 @@ contract('Crowdsale', function([_, wallet, tokenWallet, investor1, investor2]) {
         // crowdsale config
         this.rate = web3.utils.toBN('1'); 
         this.wallet = wallet; 
-        this.tokenWallet = tokenWallet; 
+        this.cap = ether('100'); 
+        this.investorMinCap = ether('0.002');
+        this.investorHardCap = ether('50'); 
+        //this.tokenWallet = tokenWallet; 
         this.crowdsale = await MyCrowdsale.new(
             this.rate, 
             this.wallet,
             this.token.address,
-            this.tokenWallet
+            this.cap
+            //this.tokenWallet
         );
 
         // give a tokens to tokenWallet
-        
+        // this.token.transfer(tokenWallet, this.initialSupply);
 
-
-        // transfer ownership?
-        // await this.token.transferOwnership(this.crowdsale.address);
+        // add token minter role to crowdsale contract 
+        await this.token.addMinter(this.crowdsale.address);
     });
 
     describe('crowdsale', function() {
@@ -72,18 +78,78 @@ contract('Crowdsale', function([_, wallet, tokenWallet, investor1, investor2]) {
             token.should.equal(this.token.address);
         });
 
-        it('tracks the tokenWallet', async function() {
-            const tokenWallet = await this.crowdsale.tokenWallet(); 
-            tokenWallet.should.equal(this.tokenWallet);
-        })
+        // it('tracks the tokenWallet', async function() {
+        //     const tokenWallet = await this.crowdsale.tokenWallet(); 
+        //     tokenWallet.should.equal(this.tokenWallet);
+        // })
     });
+
+    describe('minted crowdsale', function() {
+        it('mints tokens after purchase', async function() {
+            const originalTotalSupply = await this.token.totalSupply();
+            await this.crowdsale.sendTransaction({value: ether('1'), from: investor1});
+            const newTotalSupply = await this.token.totalSupply();
+            assert.isTrue(newTotalSupply > originalTotalSupply); 
+        })
+    })
+
+    describe('capped crowdsale', async function() {
+        it('has the correct hard cap', async function() {
+            const cap = await this.crowdsale.cap(); 
+            expect(cap).to.eql(this.cap); 
+        }); 
+    })
 
     describe('accepting payments', function() {
         it('should accept payments', async function() {
-            const value = ether('1'); 
+            this.ts = await this.token.totalSupply(); 
+            console.log('token total supply: ', this.ts.toString()); 
+            // this.twb = await this.token.balanceOf(tokenWallet); 
+            // console.log('tokenWallet balance: ', this.twb.toString()); 
+            const value = ether('0.5'); 
             console.log('value: ', value);
-            await this.crowdsale.sendTransaction({ value: value, from: investor1})
+            // investor1 needs to transfer money to crowdsale
+            // crowdsale spends tokens in tokenWallet and transfer to investor 1 
+            //  (tokenWallet needs to approve crowdsale)
+            //  (check that allowance(tokenWallet, crowdsale) = tokens) 
+            // console.log('tokenWallet address: ', this.tokenWallet);
+            console.log("investor1 address: ", investor1); 
+            //await this.tokenWallet.approve(this.crowdsale, 100000);
+            //await this.token.approve(tokenWallet, 100);             
+            //await this.token.increaseAllowance(this.tokenWallet, 100000); 
+            
+            const purchaser = investor2; 
+            await this.crowdsale.sendTransaction({ value: value, from: investor1});
+            await this.crowdsale.buyTokens(investor1, { value: value, from: purchaser }); 
+        });
+    });
+
+    describe('individual contribution cap', function() {
+        describe('when contribution less than minimum cap', async function() {
+            it('rejects the transaction', async function() {
+                const value = this.investorMinCap - 1; 
+                await this.crowdsale.buyTokens(investor2, { value: value, from: investor2}).should.be.rejectedWith();
+            })
         })
-    })
+        describe('when the investor has already met the minimum cap', function() {
+            it('allows below minimum contribution', async function() {
+                // first valid contribution
+                const value1 = ether('1');
+                await this.crowdsale.buyTokens(investor1, { value: value1, from: investor1});
+                // below minimum contribution
+                const value2 = '1'; 
+                await this.crowdsale.buyTokens(investor1, {value: value2, from: investor1}).should.be.fulfilled;
+            }); 
+        });
+        describe('when the investor exceeds the max cap', function() {
+            it('rejects the transaction', async function() {
+                
+            })
+        })
+    });
+    
+    
+
+
 
 });
